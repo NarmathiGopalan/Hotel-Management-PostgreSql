9558d62bb7a2ee56f9bf2d3611db8e027042bb49
NarmathiGopalan <gpn711119104708@jit.ac.in>
2022-04-24 18:08:37 +0530

    first commit

---
+++ src/Components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import 'react-router-dom'
+import back from "./images/back.jpg";
+
+function Home() {
+
+    return (
+         <div className={"background"}>
+                <img src={back} alt="back" className="back" />}
+        </div>
+    )
+}
+
+export default Home;
